bb69014d3062079da3122f6fa652f5b102ed31f6
dlitvinko <dlitvinko@objectstyle.com>
2024-01-06 19:39:18 +0300

    checking git actions in branch - Success 2

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,6 +1,6 @@
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello MainApp");
-        System.out.println("Try to run tests - Success 1");
+        System.out.println("Try to run tests - Success 2");
     }
 }
